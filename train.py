51b2122bd11f6f2a3c1d7faea98282ecd0a13415
lekamm123 <43355773+lekamm123@users.noreply.github.com>
2023-12-13 10:43:10 -0600

    included path logging

--- train.py
+++ train.py
@@ -56,6 +56,7 @@ if __name__ == "__main__":
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
+        print(" Path :",wine_path)
 
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
