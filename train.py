a811cecb67818d0543c856e3297bab74a43336a8
lekamm123 <43355773+lekamm123@users.noreply.github.com>
2023-12-15 19:30:21 -0600

    Update train.py

--- train.py
+++ train.py
@@ -29,7 +29,8 @@ if __name__ == "__main__":
     np.random.seed(40)
 
     # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
-    wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
+    # wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
+    wine_path = os.path.join("D:\\Harvard\\Courses\\mlflow\\mlflow-example", "wine-quality.csv")
     data = pd.read_csv(wine_path)
 
     # Split the data into training and test sets. (0.75, 0.25) split.
